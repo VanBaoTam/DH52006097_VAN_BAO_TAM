5926d7f8795b1bb6538b7b03a1994e9cb7a9f977
Van Bao Tam <vanbaotam187@gmail.com>
2023-11-11 15:59:36 +0700

    hoan thien

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 using namespace std;
-
-int main () { 
+bool isSoHoanThien (int a) 
+{ int temp = 0;
+    for(int i =1 ; i <= a/2; i++){
+if(a % i ==0 ) {
+     temp += a;
+}
+    }
+    return (a == temp) ? true : false;
+}
+int main () {  
 
 int a; 
 cout << "Nhap so: ";
 cin >> a ;
+if(isSoHoanThien(a)) {
+     cout << "la so hoan thien";
+
+}
+else {
+    cout << "khong la so hoan thien";
+}
 }
