f7ffd6d15f5f162b2b09bfa9ccc096a4b35e720a
Van Bao Tam <vanbaotam187@gmail.com>
2023-11-11 15:52:55 +0700

    chinhphuong

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
+bool isSoChinhPhuong (int a) 
+{
+    return ((int)sqrt(a) == sqrt(a)) ? true : false;     
 
-int main () { 
+}
+int main () {  
 
 int a; 
 cout << "Nhap so: ";
 cin >> a ;
+if(isSoChinhPhuong(a)) {
+     cout << "la so chinh phuong";
+
+}
+else {
+    cout << "khong la so chinh phuong";
+}
 }
